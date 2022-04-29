df672b605f5f888c5e86e02d259e7bfbb552d611
John Livingston <git@john-livingston.fr>
2022-04-29 15:44:10 +0200

    Formulaire: dynamique autour de recu_fiscal.

--- javascript/campagnodon.js
+++ javascript/campagnodon.js
@@ -1,3 +1,39 @@
+
+/**
+ * Met en place la gestion dynamique du formulaire de Campagnodon
+ * @param {string} formSelector Le sélecteur jQuery pour trouver le contenu du formulaire
+ */
 function campagnodon_formulaire(formSelector) {
   const $form = $(formSelector);
+
+  $form.on('click', 'input[type=checkbox][name=recu_fiscal]', () => campagnodon_formulaire_recu_fiscal($form));
+  campagnodon_formulaire_recu_fiscal($form, true);
+}
+
+/**
+ * Cette fonction s'occupe de mettre à jour le formulaire en fonction de la case à cocher recu_fiscal.
+ * À appeler à chaque affichage, et à chaque changement de valeur de la case à cocher.
+ * @param {jQuery} $form Le conteneur jQuery du formulaire
+ * @param {boolean} premier_appel Si c'est le premier appel à la fonction. Si falsey, c'est un événement suite à un recalcul.
+ */
+function campagnodon_formulaire_recu_fiscal($form, premier_appel = false) {
+  const $cb = $form.find('input[type=checkbox][name=recu_fiscal]');
+  const checked = $cb.is(':checked');
+
+  if (premier_appel) {
+    // On va noter tous les champs obligatoires, pour pouvoir restaurer la valeur.
+    $form.find('[si_recu_fiscal] input[required]').attr('recu_fiscal_obligatoire', true);
+  }
+
+  if (checked) {
+    // On affiche les zones liées
+    $form.find('[si_recu_fiscal]').show();
+    // On remet les attribut required sur les champs concernés.
+    $form.find('[si_recu_fiscal] input[recu_fiscal_obligatoire]').attr('required', true);
+  } else {
+    // On masque les zones à masquer.
+    $form.find('[si_recu_fiscal]').hide();
+    // On enlève les attributs required des champs concernés.
+    $form.find('[si_recu_fiscal] input[recu_fiscal_obligatoire]').removeAttr('required');
+  }
 }
