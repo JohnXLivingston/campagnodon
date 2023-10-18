d3e3611f13e5705ccdd6c5c7bb16b30dcfb7dc02
John Livingston <git@john-livingston.fr>
2023-10-18 11:15:22 +0200

    Fix linting.

--- prive/squelettes/contenu/campagnodon_campagne_fonctions.php
+++ prive/squelettes/contenu/campagnodon_campagne_fonctions.php
@@ -6,50 +6,50 @@
  *
  */
 
-if (!defined('_ECRIRE_INC_VERSION')){
+if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
 
 function campagnodon_campagne_souscriptions_perso_json($origine) {
-  include_spip('inc/campagnodon.utils');
-  $mode_options = campagnodon_mode_options($origine);
-  if (!is_array($mode_options) || !array_key_exists('souscriptions_optionnelles', $mode_options)) {
-    return array();
-  }
-  $result = array();
-  foreach (['don', 'adhesion'] as $type) {
-    $r = array();
-    foreach ($mode_options['souscriptions_optionnelles'] as $k => $so) {
-      if (!array_key_exists('pour', $so) || empty($so['pour'])) {
-        $r[] = $k;
-      } else if (is_array($so['pour'])) {
-        if (
-          false !== array_search($type, $so['pour'], true)
-          || false !== array_search($type.'?', $so['pour'], true)
-        ) {
-          $r[] = $k;
-        }
-      }
-    }
-    $result[$type] = $r;
-  }
+	include_spip('inc/campagnodon.utils');
+	$mode_options = campagnodon_mode_options($origine);
+	if (!is_array($mode_options) || !array_key_exists('souscriptions_optionnelles', $mode_options)) {
+		return array();
+	}
+	$result = array();
+	foreach (['don', 'adhesion'] as $type) {
+		$r = array();
+		foreach ($mode_options['souscriptions_optionnelles'] as $k => $so) {
+			if (!array_key_exists('pour', $so) || empty($so['pour'])) {
+				$r[] = $k;
+			} elseif (is_array($so['pour'])) {
+				if (
+					false !== array_search($type, $so['pour'], true)
+					|| false !== array_search($type.'?', $so['pour'], true)
+				) {
+					$r[] = $k;
+				}
+			}
+		}
+		$result[$type] = $r;
+	}
 
-  return json_encode($result);
+	return json_encode($result);
 }
 
 function campagnodon_campagne_montants_par_defaut() {
-  include_spip('inc/campagnodon.utils');
-  $r = [];
-  foreach (['don', 'adhesion'] as $type) {
-    $r[$type] = implode(',', campagnodon_montants_par_defaut($type));
-  }
-  if (campagnodon_campagne_don_recurrent()) {
-    $r['don_recurrent'] = implode(',', campagnodon_montants_par_defaut('don_recurrent'));
-  }
-  return json_encode($r);
+	include_spip('inc/campagnodon.utils');
+	$r = [];
+	foreach (['don', 'adhesion'] as $type) {
+		$r[$type] = implode(',', campagnodon_montants_par_defaut($type));
+	}
+	if (campagnodon_campagne_don_recurrent()) {
+		$r['don_recurrent'] = implode(',', campagnodon_montants_par_defaut('don_recurrent'));
+	}
+	return json_encode($r);
 }
 
-function campagnodon_campagne_don_recurrent () {
-  include_spip('inc/campagnodon.utils');
-  return campagnodon_don_recurrent_active();
+function campagnodon_campagne_don_recurrent() {
+	include_spip('inc/campagnodon.utils');
+	return campagnodon_don_recurrent_active();
 }
