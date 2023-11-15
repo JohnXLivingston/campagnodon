239a5a9c4111552cd536b6edd70e37e095327f9e
John Livingston <git@john-livingston.fr>
2023-11-15 18:52:24 +0100

    Fix typo.

--- lang/campagnodon_form_fr.php
+++ lang/campagnodon_form_fr.php
@@ -32,7 +32,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
   'recu_fiscal_explication' => 'Pour un montant de _MONTANT_€, cela ne vous coûte réellement que _COUT_€ après déduction fiscale.',
   'adhesion_explication' => 'Pour une adhésion de _MONTANT_ADHESION_€, cela ne vous coûte réellement que _COUT_ADHESION_€ après déduction fiscale.',
   'adhesion_magazine_explication' =>
-    "La montant de l'adhésion comprend l'abonnement de _MAGAZINE_PRIX_€ au magazine. "
+    "Le montant de l'adhésion comprend l'abonnement de _MAGAZINE_PRIX_€ au magazine. "
     ."Sur les _RESTANT_ADHESION_€ restants, 66% sont déductibles des impôts, dans la limite de 20% de vos revenus imposables. "
     ."Ainsi, votre adhésion ne vous coûtera que _COUT_ADHESION_€ après déduction fiscale.",
   'recu_fiscal_champs_supplementaires' => 'Ces informations sont nécessaires en vue de l\'établissement du reçu qui vous sera envoyé permettant une déduction fiscale',
